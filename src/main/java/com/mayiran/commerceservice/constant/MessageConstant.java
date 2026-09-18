ef38d062980d56a444d6a638196e4f4126dce29d
mayiran <3091095002@qq.com>
2026-09-18 20:40:45 +0800

    feat(auth): 新增 JWT 登录拦截器,未登录请求返回 401

--- src/main/java/com/mayiran/commerceservice/constant/MessageConstant.java
+++ src/main/java/com/mayiran/commerceservice/constant/MessageConstant.java
@@ -7,5 +7,5 @@ public class MessageConstant {
     public static final String LOGIN_FAILURE = "用户名或密码错误";
     public static final String ACCOUNT_LOCKED = "账号被锁定";
     public static final String UNKNOWN_ERROR = "未知错误";
-
+    public static final String NOT_LOGIN="未登录或登录已过期";
 }
