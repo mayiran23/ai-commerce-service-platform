85e33b0586d0bac76d9c8f3e735f1a383125e991
mayiran <3091095002@qq.com>
2026-09-20 16:30:34 +0800

    chore: 移除误提交的临时验证副本

--- .workbuddy/tmp-n1/src/main/java/com/mayiran/commerceservice/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.mayiran.commerceservice.service;
-
-import com.mayiran.commerceservice.dto.LoginDTO;
-import com.mayiran.commerceservice.entity.User;
-
-public interface AuthService {
-    User login(LoginDTO loginDTO);
-}
